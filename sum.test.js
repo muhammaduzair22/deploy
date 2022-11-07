90df707eaf76a0016601de3421a24374e5b5c47b
Uzair <i202341@nu.edu.pk>
2022-11-07 16:36:56 +0500

    v2

--- sum.test.js
+++ sum.test.js
@@ -5,6 +5,3 @@ const sum = require('./sum');
 test("test case1 testing sum method", () => {
     expect(sum(12, 4)).toBe(16);
 })
-test("test case1 testing sum method", () => {
-    expect(sum(12, 4)).toBe(15);
-})
